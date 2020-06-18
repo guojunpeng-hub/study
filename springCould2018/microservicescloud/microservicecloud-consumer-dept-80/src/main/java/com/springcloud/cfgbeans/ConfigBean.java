31c7c2a816100d1fb59929a73f8c08d04581f643
郭俊鹏 <379287276@qq.com>
2020-06-18 19:41:35 +0800

    ribbon轮询，随机负载均衡及故障负载均衡

--- springCould2018/microservicescloud/microservicecloud-consumer-dept-80/src/main/java/com/springcloud/cfgbeans/ConfigBean.java
+++ springCould2018/microservicescloud/microservicecloud-consumer-dept-80/src/main/java/com/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,8 @@
 package com.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +15,11 @@ public class ConfigBean { //boot -->spring applicationContext.xml---@Configurati
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
-
+    @Bean
+    public IRule myRule(){
+//        return new RandomRule();// 目的是随机算法
+      return new RetryRule();// 当有故障的，多次碰壁后，就不会再访问故障机
+    }
 }
 //    @Bean
 //    public UserService getUserService()
